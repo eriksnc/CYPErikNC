4f90d301057c93bca72a74ed31a002729481a928
eriksnc <eriknajercor@gmail.com>
2019-09-14 19:32:45 -0500

    Ejercicio 1.10 Calculo de Area y perimetro

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,7 @@
+base=int(input("ingrese la base del rectangulo:"))
+altura=int(input("ingrese la altura del rectangulo:"))
+area=base*altura
+print ("El area del rectangulo es:",area)
+perimetro=2*(base+altura)
+print("y su perimetro de:",perimetro)
+
