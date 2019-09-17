cd28e1e7ba32780a48c5cdd0091906bd7fa4e487
eriksnc <eriknajercor@gmail.com>
2019-09-17 17:19:08 -0500

    se subio el programa 1.3

---
+++ libro/Promedio.py
@@ -0,0 +1,14 @@
+matricula=int(input ("Ingrese su matricula:"))
+print(matricula)
+Mat1=float(input("ingresa calificacion 1:"))
+Mat2=float(input("ingresa calificacion 2:"))
+Mat3=float(input("ingresa calificacion 3:"))
+Mat4=float(input("ingresa calificacion 4:"))
+Mat5=float(input("ingresa calificacion 5:"))
+promedio=(Mat1+Mat2+Mat3+Mat4+Mat5)/(5)
+
+if(promedio<=5):
+    print("Matricula:",matricula,"el almuno reprobo")
+else:
+    print("Matricula:",matricula," El promedio del alumno es de",promedio)
+
