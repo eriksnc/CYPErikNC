786474b69e6e8903be24af68147033521e5a5a44
eriksnc <eriknajercor@gmail.com>
2019-09-19 22:59:22 -0500

    Se modifico el problema 1.5

--- libro/Problemas/capitulo1/problema1_5.py
+++ libro/Problemas/capitulo1/problema1_5.py
@@ -3,4 +3,4 @@ radio=float(input("Radio="))
 altura=float(input("Altura="))
 volumen=3.141592*(radio**2)*altura
 area=2*3.141592*radio*altura
-print(f"El volumen del cilindro es: {volumen} y su area es:{area}")
+print(f"El volumen del cilindro es={volumen} y su area es={area}")
