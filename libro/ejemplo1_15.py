5d4ce37d04442f03371dcbbbd482ce20e52fec78
eriksnc <eriknajercor@gmail.com>
2019-09-19 22:38:19 -0500

    Ejemplos 1.13 al 1.15 terminados

---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,6 @@
+base=float(input("Ingrese la base del rectangulo:"))
+altura=float(input("Ingrese la altura del rectangulo:"))
+area=base*altura
+perimetro=2*(base+altura)
+print("Los datos del rectangulo son:")
+print(f"El Area={area} Perimetro= {perimetro}")
